7f003fd8b07f28464c0188130bb84dabdfeedbd4
MalteFP <maltefp@outlook.com>
2026-01-31 18:57:51 +0100

    Fixed stacking sorting algorithms

--- sortingAlgorithm/src/Main.java
+++ sortingAlgorithm/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         int[] arr = ArrayMaker.randomLinearArray(200);
-        new BubbleSort(arr,1);
-        new CocktailShakerSort(arr,1);
+        new BubbleSort(arr.clone(),1);
+        new CocktailShakerSort(arr.clone(),1);
 
     }
 }
